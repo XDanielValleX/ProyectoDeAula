ebfd188a39f122ef42ec1cc2fae004cf132af619
essayzs <estebano0713@gmail.com>
2024-11-24 05:46:07 -0500

    pequeños cambios en panelEstudiantes MenuProfesores

---
+++ src/co/edu/Unicolombo/s3/poo/Proyecto_De_Aula/Domain/Model/AlumnosDestacados.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/licenseprivatedefault.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package co.edu.Unicolombo.s3.poo.Proyecto_De_Aula.Domain.Model;
+
+/**
+ *
+ * @author daniel
+ */
+public class AlumnosDestacados {
+    private float notasAltas;
+    
+    public AlumnosDestacados() {
+        
+    }
+
+    public AlumnosDestacados(float notasAltas) {
+        this.notasAltas = notasAltas;
+    }
+
+    public float getNotasAltas() {
+        return notasAltas;
+    }
+
+    public void setNotasAltas(float notasAltas) {
+        this.notasAltas = notasAltas;
+    }
+
+    @Override
+    public String toString() {
+        return "AlumnosDestacados{" + "notasAltas=" + notasAltas + '}';
+    }
+    
+    
+}
